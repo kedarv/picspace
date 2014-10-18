7be900df7915fbd8d6b9eb83097813582539dcab
Nicky Semenza <14nicholasse@gmail.com>
2014-10-18 14:37:45 -0400

    map embed kinda working

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -1,38 +1,34 @@
 @extends('layout')
   
-    <!--@section('title')
-      Simple Map
-    @stop-->
-  <!--<meta name="viewport" content="initial-scale=1.0, user-scalable=no">
-    <meta charset="utf-8">-->
+
   @section('append_header')
-    <style type="text/css">
-      html, body, #map-canvas { height: 100%; margin: 0; padding: 0;}
-    </style>
-    <script type="text/javascript"
-      src=" https://maps.googleapis.com/maps/api/js">
-    </script>
-    <script type="text/javascript">
-      $(document).ready(function() {
 
-        var map;
-        function initialize() {
-          var mapOptions = {
-            zoom: 8,
-            center: new google.maps.LatLng(-34.397, 150.644),
-            mapTypeId: AIzaSyDVLb2EpDmeojFZr1pM8orGkg_lTmIxDyE
-          };
-          map = new google.maps.Map(document.getElementById('map-canvas'),
-                                    mapOptions);
-        }
+        <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
+        <script>
+    function initialize() {
+      var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+      var mapOptions = {
+        zoom: 4,
+        center: myLatlng
+      }
+      var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-        google.maps.event.addDomListener(window, 'load', initialize);
+      var marker = new google.maps.Marker({
+          position: new google.maps.LatLng(-15.363882,131.044922),
+          map: map,
+          title: 'Hello World!'
+      });
+       var marker_2 = new google.maps.Marker({
+                position: new google.maps.LatLng(15.363882,131.044922),
+                map: map,
+                title: 'Hello World!'
+            });
+    }
 
-        });
+    google.maps.event.addDomListener(window, 'load', initialize);
 
-    </script>
-  @stop
+        </script>
   @section('content')
-    <div id="map-canvas"></div>
+    <div id="map-canvas" style="width:500px;height:380px;"></div>
   @stop
 @stop

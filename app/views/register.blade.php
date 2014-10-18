acdc6a911705faded1984847fa15104a4691d068
bsarkar16 <bsarkar@purdue.edu>
2014-10-18 16:42:33 -0400

    Working Version 1.0

--- app/views/register.blade.php
+++ app/views/register.blade.php
@@ -5,26 +5,40 @@
 
         <script src="https://maps.googleapis.com/maps/api/js?v=3.exp"></script>
         <script>
+         var json = '{"current":{"lon":10,"lat":10},"drawings":[{"lon":20,"lat":20,"name":"thing0","id":"id_0"},{"lon":30,"lat":30,"name":"name thing1","id":"id_1"},{"lon":40,"lat":40,"name":"name thing2","id":"id_2"}]}';
+      var locations = JSON.parse(json);
     function initialize() {
-      var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
+      var myLatlng = new google.maps.LatLng(locations.current.lon,locations.current.lat);
       var mapOptions = {
         zoom: 4,
         center: myLatlng
       }
+     
       var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
-
-      var marker = new google.maps.Marker({
-          position: new google.maps.LatLng(-15.363882,131.044922),
+      var infowindow = new google.maps.InfoWindow();
+      var marker;
+      var i = 0; 
+      console.log(locations.drawings.length);
+      for(i = 0;i < locations.drawings.length; i++)
+      {
+         marker = new google.maps.Marker({
+          position: new google.maps.LatLng(locations.drawings[i].lon,locations.drawings[i].lat),
           map: map,
           title: 'Hello World!'
-      });
-       var marker_2 = new google.maps.Marker({
-                position: new google.maps.LatLng(15.363882,131.044922),
-                map: map,
-                title: 'Hello World!'
-            });
+        });console.log(locations.drawings[i].lon);
+         console.log(locations.drawings[i].lat);
+         console.log(locations.drawings[i].name);
+         google.maps.event.addListener(marker, 'click', (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations.drawings[i].name);
+          infowindow.open(map, marker);
+        }
+      })(marker, i));
+      }
+       
+       
     }
-
+    
     google.maps.event.addDomListener(window, 'load', initialize);
 
         </script>

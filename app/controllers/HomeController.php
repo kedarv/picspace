97ccc7f563675e3b086f5b15dbc5f31f8190c83e
Nicky Semenza <14nicholasse@gmail.com>
2014-10-18 16:16:31 -0400

    id routes

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -21,11 +21,34 @@ class HomeController extends BaseController {
 	}
 	public function register()
 	{
-		return View::make('register');
+
+        $data = Array();
+        $points = Array();
+        $points['current']['lon'] = 0;
+        $points['current']['lat'] = 0;
+
+
+
+
+        for($x=0; $x< 3; $x++)
+        {
+            $arr = array("lon" => 0,
+                "lat" => 0,
+            "name" => 'name thing'.$x,
+            "id" => 'id_'.$x);
+            $points['drawings'][] = $arr;
+        }
+
+        $data['json']=json_encode($points);
+        var_dump($data);
+
+		return View::make('register', compact('data'));
 	}
-    public function draw1()
+    public function draw1($drawing_id)
     {
-        return View::make('draw1');
+        $data = array();
+        $data['drawing_id']=$drawing_id;
+        return View::make('draw1', compact('data'));
     }
     public function draw2()
     {

07acf2c2225cd85844251b79dfa1fcd72b6e075e
Kedar <kedar54@gmail.com>
2014-10-18 12:49:00 -0400

    firebase push enabled

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -20,41 +20,51 @@ var myFirebaseRef = new Firebase("https://picspace.firebaseio.com/session1");
 </script>
 <div id="editor"></div>
 <script type="text/javascript">
-$(document).ready(function() {
-var sketchpad = Raphael.sketchpad("editor", {
-	width: 400,
-	height: 400,
-	editing: true
-});
-
-// When the sketchpad changes, update the input field.
-sketchpad.change(function() {
-	var data_json = sketchpad.json();
-	myFirebaseRef.push({
-			data: data_json,
+	$(document).ready(function() {
+		var sketchpad = Raphael.sketchpad("editor", {
+			width: 400,
+			height: 400,
+			editing: true
 		});
-	console.log(data_json);
-});
-});
+
+		// When the sketchpad changes, update the input field.
+		sketchpad.change(function() {
+			var data_json = sketchpad.json();
+			myFirebaseRef.push({
+					data: data_json,
+				});
+			console.log(data_json);
+		});
+	});
 </script>
 <div id="viewer"></div>
 
 <script type="text/javascript">
-myFirebaseRef.on('value', function (snapshot) {
-console.log(snapshot.val());
-}, function (errorObject) {
-console.log('The read failed: ' + errorObject.code);
-});
-var strokes = [{
-	type:"path",
-	path:[["M",10,10],["L",390,390]],
-	fill:"none", "stroke":"#000000",
-}];
-var sketchpad = Raphael.sketchpad("viewer", {
-	width: 400,
-	height: 400,
-	strokes: strokes,
-	editing: false
-});
+	myFirebaseRef.on('value', function (snapshot) {
+		returned_value = snapshot.val();
+		//console.log(returned_value);
+		var arr = [];
+		var i = 0;
+		$.each(returned_value, function( key, value ) {
+			$.each(value, function( k, v ) {
+				arr[i] = v;
+				i++;
+			});
+		});
+		console.log(arr);
+		var strokes = [{
+			type:"path",
+			path: arr,
+			fill:"none", "stroke":"#000000",
+		}];
+		var sketchpad = Raphael.sketchpad("viewer", {
+			width: 400,
+			height: 400,
+			strokes: strokes,
+			editing: false
+		});
+	}, function (errorObject) {
+		console.log('The read failed: ' + errorObject.code);
+	});
 </script>
 @stop

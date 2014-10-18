58e4e97b1932257e89d7f3dab02e554b2db144b6
Kedar <kedar54@gmail.com>
2014-10-18 13:40:44 -0400

    further confide integration

--- app/views/nav.blade.php
+++ app/views/nav.blade.php
@@ -7,13 +7,29 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            {{ HTML::linkAction('home', 'Pic Space', array(), array('class' => 'navbar-brand')) }}
+            {{ HTML::linkAction('home', 'PicSpace', array(), array('class' => 'navbar-brand')) }}
         </div>
 
         <div class="collapse navbar-collapse" id="nav-collapse">
             <ul class="nav navbar-nav">
-                <li><a href="#">something</a></li>
+                <li><a href="{{route('home')}}">Raphael</a></li>
+                <li><a href="{{action('HomeController@draw1')}}">Draw1</a></li>
             </ul>
+            <ul class="nav navbar-nav navbar-right">
+             @if (Auth::guest())
+                <li><a href="{{action('UsersController@create')}}">Register</a></li>
+                <li><a href="{{action('UsersController@login')}}">Login</a></li>
+             @else
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu" role="menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                    </ul>
+                </li>
+            </ul>
+            @endif
         </div>
         <!-- /.navbar-collapse -->
     </div>

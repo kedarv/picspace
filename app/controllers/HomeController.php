061e08688193ad25bc7e488a1848cab1334a1555
Kedar <kedar54@gmail.com>
2014-10-18 21:24:49 -0400

    added checkEditable

--- app/controllers/HomeController.php
+++ app/controllers/HomeController.php
@@ -1,7 +1,21 @@
 <?php
 
 class HomeController extends BaseController {
-
+    // Outputs if user is within allowed range
+    public function checkEditable($myLat, $myLng, $getLat, $getLng) {
+        $earth_radius = 6371;
+        $dLat = deg2rad($latitude2 - $latitude1);
+        $dLon = deg2rad($longitude2 - $longitude1);
+        $a = sin($dLat/2) * sin($dLat/2) + cos(deg2rad($latitude1)) * cos(deg2rad($latitude2)) * sin($dLon/2) * sin($dLon/2);
+        $c = 2 * asin(sqrt($a));
+        $d = $earth_radius * $c * .62137;
+        if($d < 1) {
+            return $editable = true;
+        }
+        else {
+            return $editable = false;
+        }
+    }
 	/*
 	|--------------------------------------------------------------------------
 	| Default Home Controller
@@ -65,6 +79,7 @@ class HomeController extends BaseController {
         //$data['drawing_id']=$drawing_id;
         $data=$drawings['data'];
         $data['key']=$drawing_id;
+        $data['editable'] =  $this->checkEditable($_SESSION['lat'], $_SESSION['lon'], $data['lat'], $data['lon']);
         return View::make('draw', compact('data'));
     }
     public function locationPost() {

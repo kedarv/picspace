3276cc56d7ec0e914b88d332b36abe9de61c3f27
JamesWalts <jwalts@purdue.edu>
2014-10-18 22:03:45 -0400

    Facebook like button

--- app/views/draw.blade.php
+++ app/views/draw.blade.php
@@ -12,6 +12,14 @@
 
 
 @section('content')
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/en_US/sdk.js#xfbml=1&version=v2.0";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
 <style type="text/css">
   #picker { width: 200px; height: 200px }
   #slide { width: 30px; height: 200px }
@@ -119,10 +127,11 @@ ColorPicker(
 
   });
 </script>
-
+<div class="fb-like" data-href="https://developers.facebook.com/docs/plugins/" data-layout="standard" data-action="like" data-show-faces="true" data-share="true"></div>
 <script type="text/javascript">
 
                 </script>
                 <br>
+              
 
 @stop

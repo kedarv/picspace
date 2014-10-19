c296daa568cfd09a0e08a95eea3c8b53d8bfb7f2
Kedar <kedar54@gmail.com>
2014-10-18 21:05:28 -0400

    Added circle radius

--- app/views/home.blade.php
+++ app/views/home.blade.php
@@ -12,31 +12,36 @@
       function initialize() {
         var myLatlng = new google.maps.LatLng({{Session::get('lat')}},{{Session::get('lon')}});
         var mapOptions = {
-          zoom: 14,
+          zoom: 15,
           center: myLatlng,
-                  disableDefaultUI: true,
-        scrollwheel: false,
-   		navigationControl: false,
-    	mapTypeControl: false,
-    	scaleControl: false,
-    	draggable: false,
+            disableDefaultUI: false,
+        	scrollwheel: false,
+   			navigationControl: false,
+    		mapTypeControl: false,
+    		scaleControl: true,
+    		draggable: true,
         }
+
         var map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
         var infowindow = new google.maps.InfoWindow();
         var marker;
         var i = 0; 
       //console.log(locations.drawings.length);
-        for(i = 0;i < locations.drawings.length; i++)
-        {
-          marker = new google.maps.Marker({
-          position: new google.maps.LatLng(locations.drawings[i].lat,locations.drawings[i].lon),
-          map: map,
-          title: 'Hello World!'
-        });
+          	myposition = new google.maps.Marker ({
+          		position: new google.maps.LatLng({{Session::get('lat')}},{{Session::get('lon')}}),
+          		map: map,
+         		title: 'My Position'
+        	});
+        for(i = 0;i < locations.drawings.length; i++) {
+        	var name = locations.drawings[i].name;
+          	marker = new google.maps.Marker ({
+          		position: new google.maps.LatLng(locations.drawings[i].lat,locations.drawings[i].lon),
+          		map: map,
+         		title: name
+        	});
         //console.log(locations.drawings[i].lon);
         //console.log(locations.drawings[i].lat);
         //console.log(locations.drawings[i].name);
-        var name = locations.drawings[i].name;
         google.maps.event.addListener(marker, 'click', (function(marker, i) {
           return function() {
             infowindow.setContent(name.link(locations.drawings[i].link));
@@ -44,6 +49,16 @@
           }
         })(marker, i));
       }
+      	var circle = new google.maps.Circle({
+  			map: map,
+  			radius: 1609,    // 10 miles in metres
+		strokeColor: '#0033CC',
+      strokeOpacity: 0.5,
+      strokeWeight: 1,
+      fillColor: '#0066FF',
+      fillOpacity: 0.1,
+		});
+circle.bindTo('center', myposition, 'position');
     }
     google.maps.event.addDomListener(window, 'load', initialize);
     </script>
